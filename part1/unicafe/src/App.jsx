be26a98d5ed69c0d07e177d2956746427797b5be
Ker Shuen <kershuen01@gmail.com>
2025-03-29 21:53:09 +0800

    Added part 1 (1.7)

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,12 +1,6 @@
 import { useState } from 'react'
 
-const Header = ( {title} ) => {
-  return (
-    <h1>
-      {title}
-    </h1>
-  )
-}
+const Header = ( {title} ) => <h1>{title}</h1>
 
 const PrintClicks = ({ good, neutral, bad }) => { 
   return (
@@ -18,68 +12,38 @@ const PrintClicks = ({ good, neutral, bad }) => {
   )
 }
 
+const Button = (props) => <button onClick={props.onClick}> {props.text} </button>
+
+const All = ( {all} ) => {
+  if (all === 0) {
+    return <div>No feedback given</div>
+  }
+  return <div> all {all}</div>
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [total, setTotal] = useState(0)
-  const [score, setScore] = useState(0)
-  const [average, setAverage] = useState(0)
-  const [positive, setPositive] = useState(0)
 
-  const clickGood = () => {
-    const updatedGood = good + 1
-    const updatedScore = score + 1
-    const updatedTotal = total + 1
+  var all = good + neutral + bad
+  var average = (good - bad) / all 
+  var positive = (good / all) * 100
 
-    setGood(updatedGood)
-    console.log('Good before', good)
-    setTotal(updatedGood + neutral + bad)
-    setScore(updatedScore)
-    setAverage(updatedScore/updatedTotal)
-    console.log(updatedTotal)
-    setPositive((updatedGood / updatedTotal) * 100)
-  }
-
-  const clickNeutral = () => {
-    const updatedNeutral = neutral + 1
-    const updatedTotal = total + 1
-
-    setNeutral(updatedNeutral)
-    console.log('Neutral before', neutral)
-    setTotal(good + updatedNeutral + bad)
-    setAverage(score/updatedTotal)
-    console.log(total)
-    setPositive((good / updatedTotal) * 100)
-  }
-
-  const clickBad = () => {
-    const updatedBad = bad + 1
-    const updatedScore = score - 1
-    const updatedTotal = total + 1
-
-    setBad(updatedBad)
-    console.log('Bad before', bad)
-    setTotal(good + neutral + updatedBad)
-    setScore(updatedScore)
-    setAverage(updatedScore/updatedTotal)
-    console.log(total)
-    setPositive((good / updatedTotal) * 100)
-  }
+  console.log("Good:", good, "Neutral:", neutral, "Bad:", bad, "All:", all, "Average:", average);
 
   return (
     <div>
       <Header title="give feedback"/>
-      <button onClick= {() => clickGood(good + 1)}>good</button>
-      <button onClick= {() => clickNeutral(neutral + 1)}>neutral</button>
-      <button onClick= {() => clickBad(bad + 1)}>bad</button>
+      <Button onClick= {() => setGood(good + 1)} text="good" />
+      <Button onClick= {() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick= {() => setBad(bad + 1)} text="bad"/>
       <Header title="statistics"/>
       <PrintClicks good={good} neutral={neutral} bad={bad} />
-      <p>total {total}</p>
+      <All all={all}/>
       <p>average {average}</p>
       <p>positive {positive}%</p>
-      
     </div>
   )
 }
