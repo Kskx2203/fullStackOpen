c9a97ab9639da2e4948f3122361048cdec353f74
Ker Shuen <kershuen01@gmail.com>
2025-04-05 22:07:23 +0800

    added 2.5

--- part2/courseinfo2/src/App.jsx
+++ part2/courseinfo2/src/App.jsx
@@ -1,3 +1,5 @@
+import Course from './Course.jsx' // Importing Course module which has Part component
+
 const App = () => {
   const courses = [
     {
@@ -44,33 +46,12 @@ const App = () => {
     }
   ]
 
-  // As a course has many parts, a component is made to extract each part's name and exercise (2.1)
-  const Part = ({ parts }) => <p>{parts.name} {parts.exercises}</p>
+  // Moved Part into Course module (2.5)
 
   // const totalExercises = course.parts.reduce((total, part) => total + part.exercises, 0)
-  // !!! Inserted into Course as value is only used in  the Course component. (2.9)
+  // !!! Inserted into Course as value is only used in  the Course component. (2.4)
 
-  // 1. Display course name as header (2.1)
-  // 2. Display each part as a separate component and involving part.id as to have unique key value for React. (2.1)
-  const Course = ({ course } ) => {
-
-    // 1. Returns the sum of all exercises of a course. (2.2)
-    // 2. When used console.log(total, part), the last total is not displayed 
-    // on console.log as there isnt anymore items to go through. (2.2)
-    // 3. 2 iterations displayed when console.log is used bacause of strict mode to help with debugging. (2.2)
-    // 4. Reduce is a function for arrays, can take in a callback function. (2.2)
-    const totalExercises = course.parts.reduce((total, part) => total + part.exercises, 0)
-
-    return (
-      <>
-        <h2>{course.name}</h2>
-        {course.parts.map(part =>
-          <Part key={part.id} parts={part}/>
-        )}
-        <b>total of {totalExercises} exercises</b>
-      </>
-    )
-  }
+  // Moved Course into its own module (2.5)
 
   // Displaying all courses
   return (
