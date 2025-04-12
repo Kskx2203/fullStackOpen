264f19c80c902225fabea5a4abc05b4f089a0ede
Ker Shuen <kershuen01@gmail.com>
2025-04-12 18:37:46 +0800

    added 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,43 +2,39 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', id: "1"} // Added id for consistency (2.6)
-  ]) 
+    { name: 'Arto Hellas', id: "1", phone: "111-1234567"} // Added phone (2.8)
+  ])
 
-  // (used onChange) To control the input field and know what the user wants to add (2.6)
-  // If this state is not included, users will not be able to change the input field (2.6)
   const [newName, setNewName] = useState('banana')
+  const [newPhone, setNewPhone] = useState('222-1234567')
 
-  // To add new person by user (2.6)
   const addPerson = (event) => {
-    // Used to prevent the page from rerendering (page will rerender by default on submit). (2.6)
     event.preventDefault()
 
-    // Preventing duplicate names from being added to the phonebook (2.7)
     if (persons.some(person => person.name === newName)) {
       alert(`${newName} is already added to phonebook`)
     } else {
-      // nameObject is created to receive contents from newName state (2.6)
       const nameObject = { 
         name: newName,
+        phone: newPhone,
         id: String(persons.length + 1)
       }
 
-      // .concat does not mutate the original notes array but create a new copy (2.6)
       setPersons(persons.concat(nameObject))
 
-      // Reset newName
       setNewName('') 
+      setNewPhone('') // Reset phone on input (2.8)
     }
   }
 
-  // onChange doesn't require preventDefault() because it doesn't trigger a browser event that needs cancellation — it just listens to input changes.(2.6)
-  // hence, no event.preventDefault() needed in handleNameCHange to handle the event(onChnage)
   const handleNameChange = (event) => {
     console.log(event.target.value)
-
-    // Every keystroke updates newName using setNewName (2.6)
     setNewName(event.target.value)
+  }
+
+  const handlePhoneChange = (event) => {
+    console.log(event.target.value)
+    setNewPhone(event.target.value)
   }
   
   return (
@@ -49,13 +45,16 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
+          number: <input value={newPhone} onChange={handlePhoneChange}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
 
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <li key={person.id}>{person.name}</li>
+        <li key={person.id}>{person.name} {person.phone}</li>
       ))}
     </div>
   )
